a7436572f9184985a2efed0d4c378da1058d51a4
Vladimir Kivernik <vladimir@kivernik.ru>
2021-01-17 11:30:12 +0300

    Move repeated config elements to the separate pkg

--- cmd/executor/config.go
+++ cmd/executor/config.go
@@ -1,23 +1,9 @@
 package main
 
+import "github.com/vladimirdotk/news-bot/config"
+
 // Config describes configuration for (command) executor.
 type Config struct {
-	Telegram Telegram
-	Redis    Redis
-}
-
-// Telegram configures telegram options.
-type Telegram struct {
-	// BotToken sets token for bot given by BotFather
-	BotToken string `env:"TELEGRAM_BOT_TOKEN,required"`
-}
-
-// Redis configures redis options.
-type Redis struct {
-	// Addr sets host(or ip) and optional port for redis connection.
-	Addr string `env:"REDIS_ADDR,required"`
-	// Password sets redis password, optional.
-	Password string `env:"REDIS_PASSWORD"`
-	// Db sets DB number, optional.
-	Db int `env:"REDIS_DB"`
+	Telegram config.Telegram
+	Redis    config.Redis
 }
---
+++ config/telegram.go
@@ -0,0 +1,7 @@
+package config
+
+// Telegram configures telegram options.
+type Telegram struct {
+	// BotToken sets token for bot given by BotFather
+	BotToken string `env:"TELEGRAM_BOT_TOKEN,required"`
+}
